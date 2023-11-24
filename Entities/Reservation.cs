d55ac5d571c4d98fd42eb3a3e0f38fe1d01916bb
joao vitor <githubconta2021@gmail.com>
2023-11-24 11:58:29 -0300

    first commit

---
+++ Entities/Reservation.cs
@@ -0,0 +1,34 @@
+namespace Program.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set;}
+        public DateTime CheckOut { get; set; }
+        public Reservation()
+        {}
+        public Reservation(int roomNumber,DateTime checkin, DateTime checkOut)
+        {
+            RoomNumber = roomNumber; 
+            CheckIn = checkin; 
+            CheckOut = checkOut;
+        }
+        public int Duration()
+        {
+            TimeSpan durantion = CheckOut.Subtract(CheckIn);
+            return (int)durantion.TotalDays;
+        }
+        public override string ToString()
+        {
+            return "Room" 
+            + RoomNumber
+            + "Check-in: "
+            + CheckIn.ToString("DD/MM/YYYY")
+            + "Check-out: "
+            + CheckOut.ToString("DD/MM/YYYY")
+            + ","
+            + Duration() 
+            + " nights"; 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main()
+        {
+            // entrada de dados 
+
+            Console.WriteLine("Room number: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Check-in: (DD/MM/YYYY) ");
+            DateTime checkin = DateTime.Parse(Console.ReadLine());
+ 
+            // processsamento de dados  
+
+            // saida de dados 
+        }
+   }
+}
+
